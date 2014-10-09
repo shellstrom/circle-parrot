190f2d8261469f2fee68ec2993062d9bc8ce80a1
Peter <peter.bartos@sonymobile.com>
2014-10-09 15:05:37 +0200

    Preparing super special custom UI for SmartEyeglass

--- src/com/awesomesaucy/drone/extension/SEGControl.java
+++ src/com/awesomesaucy/drone/extension/SEGControl.java
@@ -1,15 +1,14 @@
 package com.awesomesaucy.drone.extension;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 
-import com.parrot.freeflight.R;
-import com.parrot.freeflight.activities.ControlDroneActivity;
-import com.parrot.freeflight.receivers.DroneEmergencyChangeReceiver;
 import com.parrot.freeflight.service.DroneControlService;
+import com.parrot.freeflight.ui.gl.GLBGVideoSprite;
 import com.sonyericsson.extras.liveware.aef.control.Control;
 import com.sonyericsson.extras.liveware.extension.util.control.ControlExtension;
 import com.sonyericsson.extras.liveware.extension.util.control.ControlTouchEvent;
@@ -19,36 +18,61 @@ class SEGControl extends ControlExtension {
 	private static String TAG = "AwesomeDrone";
 	
 	private Context context;
-
-    long mLastTouch;
-    boolean flyMode = false;
-    boolean isFlying = false;
-    boolean isHovering = false;
 	
     SEGControl(final String hostAppPackageName, final Context context) {
         super(context, hostAppPackageName);
         this.context = context;
     }
     
+    private Bitmap hudBmp;
+    
+    private Bitmap getHUD()
+    {
+    	int w = Helper.getSmartEyeglassWidth(mContext);
+    	int h = Helper.getSmartEyeglassHeight(mContext);
+
+    	float yaw = 0.5f; // left right speed
+    	float gaz = 0.3f; // speed
+
+    	// Free last frame
+    	if (hudBmp != null) {
+    		hudBmp.recycle();
+    		hudBmp = null;
+    	}
+    	
+    	// Try to get drone video frame
+    	synchronized (GLBGVideoSprite.videoFrameLock)
+    	{
+    		if (GLBGVideoSprite.video != null)
+    		{
+    			hudBmp = Bitmap.createScaledBitmap(GLBGVideoSprite.video, w, h, true);
+    		}
+    	}
+    	
+    	// Blank bitmap if no video frame available
+    	hudBmp = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+    	hudBmp.eraseColor(Color.BLACK);
+    	
+    	Paint paint = new Paint();
+    	paint.setAntiAlias(true);
+    	paint.setColor(Color.WHITE);
+    	paint.setStyle(Paint.Style.STROKE);
+    	paint.setStrokeWidth(2);
+    	
+    	Canvas canvas = new Canvas(hudBmp);
+    	canvas.drawCircle(w/2, h/2, h/2, paint);
+    	canvas.drawLine(0, h/2, w, h/2, paint);
+    	
+    	return hudBmp;
+    }
+    
     @Override
     public void onResume() {
         Log.d(ExtensionDroneService.LOG_TAG, "Starting control");
         setScreenState(Control.Intents.SCREEN_STATE_DIM);
-        showLayout(R.layout.seg_layout, null);
-        
-        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(context);
-        localBroadcastMgr.registerReceiver(emergencyReceiver, new IntentFilter(DroneControlService.DRONE_EMERGENCY_STATE_CHANGED_ACTION));
+        showBitmap(getHUD());        
     }
     
-    private DroneEmergencyChangeReceiver emergencyReceiver = new DroneEmergencyChangeReceiver(ControlDroneActivity.controlDroneActivity){
-    	@Override
-    	public void onReceive(Context context, Intent intent) 
-    	{
-    		//int code = intent.getIntExtra(DroneControlService.EXTRA_EMERGENCY_CODE, 0);
-    		//TODO
-    	}
-    };
-
     @Override
     public void onTouch(ControlTouchEvent event) {
         super.onTouch(event);
@@ -66,7 +90,7 @@ class SEGControl extends ControlExtension {
     public void onSwipe(int direction) {
     	super.onSwipe(direction);
     	try{
-        	ControlDroneActivity.droneControlService.doLeftFlip();
+        	DroneControlService.instance.doLeftFlip();
         }catch(Exception e){
         	Log.e(TAG, "exception in onTouch");
         	e.printStackTrace();
